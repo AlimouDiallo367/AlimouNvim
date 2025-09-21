0f406b07258921e500e6ca425a5e1f20d752dc78
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-09-20 20:41:40 -0400

    New LSP API

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,33 +1,47 @@
 return {
   {
     "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
+    opts = {},
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer", "ruby_lsp", "omnisharp" },
-      })
-    end,
+    opts = {
+      ensure_installed = { "lua_ls", "ts_ls", "rust_analyzer", "ruby_lsp", "omnisharp" },
+    },
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.ts_ls.setup({ capabilities = capabilities })
-      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
-      lspconfig.ruby_lsp.setup({ capabilities = capabilities })
-      lspconfig.omnisharp.setup({
-        cmd = { vim.fn.stdpath("data") .. "/mason/bin/OmniSharp" },
+      -- Lua
+      vim.lsp.config("lua_ls", {
         capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
       })
+      vim.lsp.enable("lua_ls")
 
+      -- TypeScript
+      vim.lsp.config("ts_ls", { capabilities = capabilities })
+      vim.lsp.enable("ts_ls")
+
+      -- Rust
+      vim.lsp.config("rust_analyzer", { capabilities = capabilities })
+      vim.lsp.enable("rust_analyzer")
+
+      -- Ruby
+      vim.lsp.config("ruby_lsp", { capabilities = capabilities })
+      vim.lsp.enable("ruby_lsp")
+
+      -- C#
+      vim.lsp.config("omnisharp", { capabilities = capabilities })
+      vim.lsp.enable("omnisharp")
+
+      -- Diagnostics
       vim.diagnostic.config({
         virtual_text = true,
         signs = true,
@@ -36,6 +50,7 @@ return {
         severity_sort = true,
       })
 
+      -- Keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
